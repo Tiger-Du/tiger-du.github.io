8d3a35aa91b46b54f1ec1eec99fb99f055840b4f
Tiger Du <tigerdu@seas.upenn.edu>
2024-11-20 09:53:40 -0600

    Update reformat.py

--- reformat.py
+++ reformat.py
@@ -1,8 +1,8 @@
 import json
 
-with open('embeddings.json', 'r') as file:
+with open('./_build/html/embeddings.json', 'r') as file:
     json_data = json.load(file)
     json_data['Kind'] = "Article"
 
-with open('embeddings.json', 'w') as file:
+with open('./_build/html/embeddings.json', 'w') as file:
     file.write(json.dumps(json_data))
