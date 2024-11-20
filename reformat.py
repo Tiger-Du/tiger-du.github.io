7ab2d4ef0fe768325b17652de21af5b47b5ce7db
Tiger Du <tigerdu@seas.upenn.edu>
2024-11-20 10:11:02 -0600

    Update reformat.py

--- reformat.py
+++ reformat.py
@@ -1,8 +1,17 @@
 import json
 
+from bs4 import BeautifulSoup
+import lxml
+
 with open('./_build/html/embeddings.json', 'r') as file:
     json_data = json.load(file)
     json_data['Kind'] = "Article"
 
 with open('./_build/html/embeddings.json', 'w') as file:
     file.write(json.dumps(json_data))
+
+with open('./_build/html/embeddings.html', 'w') as file:
+    soup = BeautifulSoup(file, 'lxml')
+
+    for element in soup.find_all(class_='inline-block mr-1'):
+        element.decompose()
